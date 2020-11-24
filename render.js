2fb2765a21973825b681181a619afcb48f5c377e
Raj Gandecha <rajg@ad.unc.edu>
2020-11-24 02:34:34 -0500

    trying stuff

--- render.js
+++ render.js
@@ -128,5 +128,6 @@ async function deleteTweet(id) {
 
 async function recentTweets(){
     const result = await axios.get('https://comp426finalbackendactual2.herokuapp.com/tweets/recent', {limit: "75", skip: "0"}, {withCredentials: true});
-    return result.data.body;
+    console.log(result);
+    return result.data;
 }
