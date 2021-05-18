841db23c37464a2e3586229c5dc9969f7f026720
zookowen <owenzook@live.unc.edu>
2021-05-17 21:09:52 -0400

    return liked array

--- render.js
+++ render.js
@@ -111,6 +111,7 @@ async function getUsersTweets(userId, type) {
 
 async function getUserLikes(userId) {
   const result = await axios.get(`https://api.426twitter20.com/tweets/user/likes/${userId}`)
+  return result;
 }
 
 
