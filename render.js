0636ea6d2eb19021ff5fb100e0576fb33a1d5f00
zookowen <owenzook@live.unc.edu>
2021-05-23 19:23:01 -0400

    video resolution change test

--- render.js
+++ render.js
@@ -376,7 +376,7 @@ async function renderTweetBody(data, element, liked, reply) {
                               ${data.body}
                               <br>
                               <figure class="image has-text-left">
-                                <iframe class="has-ratio" width="100%" height="auto" src="https://www.youtube.com/embed/${data.videoId}" frameborder="0" allowfullscreen></iframe>
+                                <iframe class="has-ratio" width="640" height="auto" src="https://www.youtube.com/embed/${data.videoId}" frameborder="0" allowfullscreen></iframe>
                               </figure>
                               </div>
                           </div>
