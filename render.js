49167d1395a3a567552d45d5d813128615886bcc
zookowen <owenzook@live.unc.edu>
2021-05-22 22:36:34 -0400

    video showing techinques tests

--- render.js
+++ render.js
@@ -378,9 +378,7 @@ async function renderTweetBody(data, element, liked, reply) {
                               <div class="edit-area-${data.id}">
                               ${data.body}
                               <br>
-                              <figure class="image is-16by9">
-                                <iframe class="has-ratio" width="640" height="360" src="https://www.youtube.com/embed/${data.videoId}" frameborder="0" allowfullscreen></iframe>
-                              </figure>
+                              <iframe class="has-ratio" width="640" height="360" src="https://www.youtube.com/embed/${data.videoId}" frameborder="0" allowfullscreen></iframe>
                               </div>
                           </div>
                         </div>
